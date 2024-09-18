34e487a59456280d508293dfd3f0ff73ed2d26d2
Roxana Mihaela Baba <alu0101339887@ull.edu.es>
2024-09-18 15:46:54 +0100

    Mostrar listado de Héroes

--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
 
+
 @Component({
   selector: 'app-list-page',
   templateUrl: './list-page.component.html',
