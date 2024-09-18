cde2b5c04e8d0f34f83c3ccd853272b383545f81
Roxana Mihaela Baba <alu0101339887@ull.edu.es>
2024-09-18 15:27:39 +0100

    HeroesService

---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, map, of } from 'rxjs';
+import { Hero } from '../interfaces/hero.interface';
+import { environment } from 'src/environments/environments';
+
+@Injectable({providedIn: 'root'})
+export class HeroesService {
+
+  private baseUrl: string = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+  }
+}
+
